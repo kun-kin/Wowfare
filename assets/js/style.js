2ff8a60c8f114d13aab2f488ce45c84bbfb6bb0a
kun-kin <nguyenquyenlinhnb@gmail.com>
2023-03-24 10:55:45 +0700

    f
    d

--- assets/js/style.js
+++ assets/js/style.js
@@ -276,4 +276,27 @@ $(document).ready(function() {
         }
     });
 
+    $('.select-air').select2({
+        tags: true,
+        placeholder: 'Select or Search by Airlines'
+    })
+
+});
+
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = (() => {
+    loginForm.style.marginLeft = "-50%";
+    loginText.style.marginLeft = "-50%";
+});
+loginBtn.onclick = (() => {
+    loginForm.style.marginLeft = "0%";
+    loginText.style.marginLeft = "0%";
+});
+signupLink.onclick = (() => {
+    signupBtn.click();
+    return false;
 });
